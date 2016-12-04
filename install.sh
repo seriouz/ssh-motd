d5b3f794ab7f76b7b5a0b64dce8a83769bf5cd1a
seriouz <seriouz@gmail.com>
2016-12-04 22:09:40 +0100

    sudo fix

--- install.sh
+++ install.sh
@@ -10,9 +10,9 @@ sudo rm -r /etc/update-motd.d/
 sudo mkdir /etc/update-motd.d/
 # create dynamic files
 sudo touch /etc/update-motd.d/00-header ; sudo touch /etc/update-motd.d/10-sysinfo ; sudo touch /etc/update-motd.d/90-footer
-sudo -s 'cat motd_scripts/header.script > /etc/update-motd.d/00-header'
-sudo -s 'cat motd_scripts/body.script > /etc/update-motd.d/10-sysinfo'
-sudo -s 'cat motd_scripts/footer.script > /etc/update-motd.d/90-footer'
+sudo bash -c 'cat motd_scripts/header.script > /etc/update-motd.d/00-header'
+sudo bash -c 'cat motd_scripts/body.script > /etc/update-motd.d/10-sysinfo'
+sudo bash -c 'cat motd_scripts/footer.script > /etc/update-motd.d/90-footer'
 # make files executable
 sudo chmod +x /etc/update-motd.d/*
 # remove MOTD file
